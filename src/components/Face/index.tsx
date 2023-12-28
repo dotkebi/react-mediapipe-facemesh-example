4d81c1137a0cf2ed46586cef2c0df4d8d27f5e2f
mj <dotkebi@gmail.com>
2023-12-29 00:24:47 +0900

    :tada: begin project

---
+++ src/components/Face/index.tsx
@@ -0,0 +1,172 @@
+import {useEffect, useRef, useState} from 'react';
+
+import {
+  DrawingUtils,
+  FaceLandmarker,
+  FaceLandmarkerResult,
+  FilesetResolver
+} from '@mediapipe/tasks-vision';
+import './index.scss';
+
+const wasmFolderPath = 'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm'
+const FaceContainer = () => {
+  const [inputVideoReady, setInputVideoReady] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+
+  const inputVideoRef = useRef<HTMLVideoElement | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const contextRef = useRef<CanvasRenderingContext2D | null>(null);
+
+
+  let lastVideoTime = -1;
+  const [face, setFace] = useState<FaceLandmarker | null>();
+
+  const sendToMediaPipe = async () => {
+    if (inputVideoRef.current) {
+      if (!inputVideoRef.current.videoWidth) {
+        console.log(inputVideoRef.current.videoWidth);
+        requestAnimationFrame(sendToMediaPipe);
+      } else {
+        let startTimeMs = performance.now();
+        if (face != null) {
+          const results = face.detectForVideo(inputVideoRef.current, startTimeMs);
+          onResults(results);
+        }
+
+        requestAnimationFrame(sendToMediaPipe);
+      }
+    }
+  };
+
+  const getFace = async () => {
+    const filesetResolver = await FilesetResolver.forVisionTasks(
+        wasmFolderPath
+    )
+    return await FaceLandmarker.createFromOptions(filesetResolver, {
+      baseOptions: {
+        modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
+        delegate: "GPU"
+      },
+      outputFaceBlendshapes: true,
+      runningMode: 'VIDEO',
+      numFaces: 1
+    });
+  }
+
+  useEffect(() => {
+    if (face !== undefined) {
+      sendToMediaPipe();
+    } else {
+      getFace().then((face) => {
+        setFace(face);
+      });
+    }
+  }, [face]);
+
+  useEffect(() => {
+    if (!inputVideoReady) {
+      return;
+    }
+    if (inputVideoRef.current && canvasRef.current) {
+      console.log('rendering');
+      contextRef.current = canvasRef.current.getContext('2d');
+      const constraints = {
+        video: {width: {min: 1280}, height: {min: 720}},
+      };
+      navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+        if (inputVideoRef.current) {
+          inputVideoRef.current.srcObject = stream;
+        }
+
+      });
+    }
+  }, [inputVideoReady]);
+
+  const onResults = (results: FaceLandmarkerResult) => {
+    if (canvasRef.current && contextRef.current) {
+      setLoaded(true);
+
+      const drawingUtils = new DrawingUtils(contextRef.current);
+      contextRef.current.save();
+      contextRef.current.clearRect(
+          0,
+          0,
+          canvasRef.current.width,
+          canvasRef.current.height
+      );
+
+      if (results.faceLandmarks) {
+        for (const landmarks of results.faceLandmarks) {
+          drawingUtils.drawConnectors(
+              landmarks,
+              FaceLandmarker.FACE_LANDMARKS_TESSELATION,
+              {color: "#C0C0C070", lineWidth: 1}
+          );
+          drawingUtils.drawConnectors(
+              landmarks,
+              FaceLandmarker.FACE_LANDMARKS_RIGHT_EYE,
+              {color: "#FF3030"}
+          );
+          drawingUtils.drawConnectors(
+              landmarks,
+              FaceLandmarker.FACE_LANDMARKS_RIGHT_EYEBROW,
+              {color: "#FF3030"}
+          );
+          drawingUtils.drawConnectors(
+              landmarks,
+              FaceLandmarker.FACE_LANDMARKS_LEFT_EYE,
+              {color: "#30FF30"}
+          );
+          drawingUtils.drawConnectors(
+              landmarks,
+              FaceLandmarker.FACE_LANDMARKS_LEFT_EYEBROW,
+              {color: "#30FF30"}
+          );
+          drawingUtils.drawConnectors(
+              landmarks,
+              FaceLandmarker.FACE_LANDMARKS_FACE_OVAL,
+              {color: "#E0E0E0"}
+          );
+          drawingUtils.drawConnectors(
+              landmarks,
+              FaceLandmarker.FACE_LANDMARKS_LIPS,
+              {color: "#E0E0E0"}
+          );
+          drawingUtils.drawConnectors(
+              landmarks,
+              FaceLandmarker.FACE_LANDMARKS_RIGHT_IRIS,
+              {color: "#FF3030"}
+          );
+          drawingUtils.drawConnectors(
+              landmarks,
+              FaceLandmarker.FACE_LANDMARKS_LEFT_IRIS,
+              {color: "#30FF30"}
+          );
+        }
+      }
+      contextRef.current.restore();
+    }
+  }
+
+
+  return (
+      <div className="face-container">
+        <video
+            autoPlay
+            ref={(el) => {
+              inputVideoRef.current = el;
+              setInputVideoReady(!!el);
+            }}
+        />
+        <canvas ref={canvasRef} width={1280} height={720}/>
+        {!loaded && (
+            <div className="loading">
+              <div className="spinner"></div>
+              <div className="message">Loading</div>
+            </div>
+        )}
+      </div>
+  );
+};
+
+export default FaceContainer;
---
+++ src/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import App from './App';
+import FaceContainer from "./components/Face";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+  },
+  {
+    path: 'face',
+    element: <FaceContainer/>,
+  },
+]);
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+root.render(
+    <>
+      <RouterProvider router={router}/>
+    </>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
